3ad258f3a2397cee8870671c4af4588ff9f6ab02
neojou <neojou@gmail.com>
2024-03-30 21:45:01 +0800

    Leetcode 976
    Signed-off-by: neojou <neojou@gmail.com>

---
+++ array/976_Largest_Perimeter_Triangle/Solutions.kts
@@ -0,0 +1,12 @@
+class Solution {
+    fun largestPerimeter(nums: IntArray): Int {
+        nums.sort()
+
+        for (i in (nums.size - 1) downTo 2) {
+            if ( nums[i - 1] + nums[i - 2] > nums[i] )
+                return nums[i] + nums[i - 1] + nums[i - 2]
+        }
+        return 0
+    }
+}
+
