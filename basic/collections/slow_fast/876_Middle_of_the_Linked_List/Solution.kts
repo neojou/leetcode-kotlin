9b317691be7f7a6a7f2eebfec8f9cb8964196327
neojou <neojou@gmail.com>
2024-04-24 21:39:30 +0800

    Leetcode 876
    Signed-off-by: neojou <neojou@gmail.com>

---
+++ basic/collections/slow_fast/876_Middle_of_the_Linked_List/Solution.kts
@@ -0,0 +1,45 @@
+package com.neojou
+
+import kotlin.system.measureNanoTime
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+
+    fun print() {
+        print("[" + `val`.toString())
+        var p = next
+        while (p != null) {
+            print( ", " + p!!.`val`.toString())
+            p = p.next
+        }
+        println("]")
+    }
+}
+
+fun makeListNode(va: IntArray): ListNode? {
+    var ret: ListNode? = ListNode(va[0])
+    var p = ret
+    for (i in 1 until va.size) {
+        p!!.next = ListNode(va[i])
+        p = p!!.next
+    }
+    return ret
+}
+
+fun middleNode(head: ListNode?): ListNode? {
+    var sp = head
+    var fp = head?.next
+    while (fp != null) {
+        sp = sp?.next
+        fp = fp?.next?.next
+    }
+    return sp
+}
+
+fun main() {
+    val vlist = intArrayOf(1, 2, 3, 4, 5, 6)
+    val head = makeListNode(vlist)
+    middleNode(head)?.print()
+}
+
+
